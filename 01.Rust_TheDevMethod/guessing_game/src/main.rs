cfdac36a140264b0b4ae694c242d71b7ac936123
Ahindra D <official.ahindra@gmail.com>
2025-03-17 20:33:46 +0530

    first commit

---
+++ 01.Rust_TheDevMethod/guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    let secret_num: u8 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input...");
+        println!("Your guess: {}", guess);
+        let guess_num: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Error parsing the guessed number: {}", e);
+                continue;
+            }
+        };
+        match guess_num.cmp(&secret_num) {
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Less => println!("Too Less"),
+        }
+    }
+}
