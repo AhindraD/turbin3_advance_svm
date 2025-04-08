8003e604df6ebf30c3851339f32a18e06983cf0e
Ahindra D <official.ahindra@gmail.com>
2025-04-08 19:31:16 +0530

    pinocchio-primer

---
+++ 02.Pinocchio_FEBO/pinocchio_primer/src/lib.rs
@@ -0,0 +1,12 @@
+use pinocchio::{ProgramResult, account_info::AccountInfo, entrypoint, msg, pubkey::Pubkey};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Hello from my program!");
+    Ok(())
+}
