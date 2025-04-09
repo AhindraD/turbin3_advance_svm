9a208081ae32868e961b4e10ea469fa47f979b4d
Ahindra D <official.ahindra@gmail.com>
2025-04-10 05:21:28 +0530

    structure - instruction split

--- 02.Pinocchio_FEBO/pinocchio_primer/src/lib.rs
+++ 02.Pinocchio_FEBO/pinocchio_primer/src/lib.rs
@@ -2,3 +2,20 @@ use pinocchio::{
     ProgramResult, account_info::AccountInfo, entrypoint, program_error::ProgramError,
     pubkey::Pubkey,
 };
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let (ix_disc, instruction_data) = instruction_data
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+    match ix_disc {
+        0 => todo!(),
+        _ => Err(ProgramError::InvalidInstructionData),
+    }?;
+    Ok(())
+}
